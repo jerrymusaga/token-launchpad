424555b75d69543e67c0cf8b4512747bb348e69d
jerrymusaga <musagatechnology@gmail.com>
2025-03-12 16:41:17 +0100

    implementation of erc721 from scratch

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -1,14 +1,18 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
 
-contract Counter {
-    uint256 public number;
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
+contract Erc20Token is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC20("MyToken", "MTK")
+        Ownable(initialOwner)
+    {}
 
-    function increment() public {
-        number++;
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
---
+++ src/ERC721.sol
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.22;
+
+import "./interface/IERC721OnReceiver.sol";
+
+contract ERC721Token {
+    string public name;
+    string public symbol;
+    uint public nextTokenIdToMint;
+    address public contractOwner;
+
+    // token id => owners
+    mapping(uint => address) internal _owners;
+    //owner => token count
+    mapping(address => uint) internal _balances;
+    //token id => approved address
+    mapping(uint => address) internal _tokenApprovals;
+    //owner => (operator => yes/no)
+    mapping(address => mapping(address => bool)) internal _operatorApprovals;
+    //token id => token uri
+    mapping(uint => string) internal _tokenUris;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
+    event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
+    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
+
+    constructor(string memory _name, string memory _symbol){
+        name = _name;
+        symbol = _symbol;
+        nextTokenIdToMint = 1;
+        contractOwner = msg.sender;
+    }
+
+    function balanceOf(address _owner) public view returns(uint){
+        require(_owner != address(0), "! Addr0");
+        return _balances[msg.sender];
+    }
+
+    function ownerOf(uint _tokenId) public view returns(address){
+        return _owners[_tokenId];
+    }
+
+     function safeTransferFrom(address _from, address _to, uint256 _tokenId) public payable {
+        safeTransferFrom(_from, _to, _tokenId, "");
+    }
+
+    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes memory _data) public payable {
+        require(ownerOf(_tokenId) == msg.sender || _tokenApprovals[_tokenId] == msg.sender || _operatorApprovals[ownerOf(_tokenId)][msg.sender], "!Auth");
+        _transfer(_from, _to, _tokenId);
+        // trigger func check
+        require(_checkOnERC721Received(_from, _to, _tokenId, _data), "!ERC721Implementer");
+    }
+
+    function transferFrom(address _from, address _to, uint _tokenId) public payable {
+        // unsafe transfer without onERC721Received, used for contracts that dont implement
+        require(ownerOf(_tokenId) == msg.sender || _tokenApprovals[_tokenId] == msg.sender || _operatorApprovals[ownerOf(_tokenId)][msg.sender], "!Auth");
+        _transfer(_from, _to, _tokenId);
+    }
+
+    function approve(address _approved, uint256 _tokenId) public payable {
+        require(ownerOf(_tokenId) == msg.sender, "!Owner");
+        _tokenApprovals[_tokenId] = _approved;
+        emit Approval(ownerOf(_tokenId), _approved, _tokenId);
+    }
+
+    function setApprovalForAll(address _operator, bool _approved) public {
+        _operatorApprovals[msg.sender][_operator] = _approved;
+        emit ApprovalForAll(msg.sender, _operator, _approved);
+    }
+
+    function getApproved(uint256 _tokenId) public view returns (address) {
+        return _tokenApprovals[_tokenId];
+    }
+
+    function isApprovedForAll(address _owner, address _operator) public view returns (bool) {
+        return _operatorApprovals[_owner][_operator];
+    }
+
+    function mintTo(address _to, string memory _uri) public {
+        require(contractOwner == msg.sender, "!Auth");
+        _owners[nextTokenIdToMint] = _to;
+        _balances[_to] += 1;
+        _tokenUris[nextTokenIdToMint] = _uri;
+        emit Transfer(address(0), _to, nextTokenIdToMint);
+        nextTokenIdToMint += 1;
+    }
+
+    function tokenURI(uint256 _tokenId) public view returns(string memory) {
+        return _tokenUris[_tokenId];
+    }
+
+    function totalSupply() public view returns(uint256) {
+        return nextTokenIdToMint;
+    }
+
+    // Internal Functions
+    function _checkOnERC721Received(address from, address to, uint tokenId, bytes memory data) private returns(bool){
+        // check if to is a contract, if yes, to.code.length will always > 0
+        if(to.code.length > 0){
+            try IERC721Receiver(to).onERC721Received(msg.sender, from, tokenId, data) returns (bytes4 retval){
+                return retval == IERC721Receiver.onERC721Received.selector;
+            } catch (bytes memory reason){
+                if(reason.length == 0){
+                    revert("ERC721: transfer to non ERC721Receiver implementer");
+                } else {
+                    /// @solidity memory-safe-assembly
+                    assembly {
+                        revert(add(32, reason), mload(reason))
+                    }
+                }
+            }
+        }else {
+            return true;
+        }
+    }
+
+    //unsafe transfer
+    function _transfer(address _from, address _to, uint _tokenId) internal{
+        require(ownerOf(_tokenId) == _from, "! owner");
+        require(_to != address(0), "!ToAdd0");
+
+        delete _tokenApprovals[_tokenId];
+        _balances[_from] -= 1;
+        _balances[_to] += 1;
+        _owners[_tokenId] = _to;
+
+        emit Transfer(_from, _to, _tokenId);
+    }
+}
